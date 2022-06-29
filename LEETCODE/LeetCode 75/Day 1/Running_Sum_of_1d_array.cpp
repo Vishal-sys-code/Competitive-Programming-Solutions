370f7e78bfa81905a1279e4fd1fd6fa3080cd30c
Vishal Pandey <bishalpandey2001@gmail.com>
2022-06-29 09:48:14 +0530

    Create Running_Sum_of_1d_array.cpp

---
+++ LEETCODE/LeetCode 75/Day 1/Running_Sum_of_1d_array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
