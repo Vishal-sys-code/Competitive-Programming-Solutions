c394e63650350694425ebdeba592a2ba9f494803
Vishal Pandey <bishalpandey2001@gmail.com>
2022-10-01 08:09:52 +0530

    Create Immobile_Knight.cpp

---
+++ Codeforces/Immobile_Knight.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve() {
+    int n,m; cin>>n>>m;
+    cout << n/2+1 << " " << m/2+1 << endl;
+}
+
+int main() {
+    ll T;
+    cin >> T;
+    while (T--){
+        solve();
+    }
+    return 0;
+}
