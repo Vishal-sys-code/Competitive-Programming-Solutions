366e77d3b0a0bc3fc6e8b63daa3a61802291a9d8
Vishal Pandey <bishalpandey2001@gmail.com>
2022-10-01 12:16:14 +0530

    Create Bird_farm.cpp

---
+++ CodeChef/Bird_farm.cpp
@@ -0,0 +1,60 @@
+/*
+Author: tmleyncodes
+
+नैनं छिद्रन्ति शस्त्राणि नैनं दहति पावक: ।
+न चैनं क्लेदयन्त्यापो न शोषयति मारुत ॥
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+unordered_map<char, int> mp;
+#define lpr pair<long long int,long long int>
+#define mod 1000000007
+#define S second
+#define F first
+#define nl endl
+const long long A = 100000000000000LL, N = 228228;
+
+// INPUTS
+#define read1(a) cin>>a
+#define read2(a,b) cin>>a>>b
+#define read3(a,b,c) cin>>a>>b>>c
+
+// OUTPUTS
+#define print1(a) cout<<a
+#define print2(a,b) cout<<a<<b
+#define print3(a,b,c) cout<<a<<b<<c
+
+
+// For the fast input and outputs in cpp
+void Fast_IO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// driver code for my contests
+void solution() {
+    ll x,y,z; read3(x,y,z);
+    if(z%x == 0 and z%y == 0) cout << "ANY" << nl;
+    else if(z%x == 0) cout << "CHICKEN" << nl;
+    else if(z%y == 0) cout << "DUCK" << nl;
+    else cout << "NONE" << nl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    Fast_IO();
+    // int t = 1; // for 1 test case
+    int t; cin >> t;
+    while (t--) {
+        solution();
+    }
+    return 0;
+}
