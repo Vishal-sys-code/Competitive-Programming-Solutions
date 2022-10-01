92376d9d28a95b0bb3f0801da2e84bba5db1cf77
Vishal Pandey <bishalpandey2001@gmail.com>
2022-10-01 12:19:19 +0530

    Create reach_target.cpp

---
+++ CodeChef/reach_target.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt; cin>>tt;
+	for(int i=0;i<tt;i++){
+	    int a,b;
+	    cin>>a>>b;
+	    cout << abs(a-b) << endl; 
+	}
+	return 0;
+}
