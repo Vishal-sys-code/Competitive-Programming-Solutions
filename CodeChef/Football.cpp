b03d724f390f0cc905fa6e82b9fac38bc78d29ad
Vishal Pandey <bishalpandey2001@gmail.com>
2022-10-02 13:39:32 +0530

    Create Football.cpp

---
+++ CodeChef/Football.cpp
@@ -0,0 +1,78 @@
+/*
+Author: tmleyncodes
+
+नैनं छिद्रन्ति शस्त्राणि नैनं दहति पावक: ।
+न चैनं क्लेदयन्त्यापो न शोषयति मारुत ॥
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+unordered_map<char, int> mp;
+#define lpr pair<long long int,long long int>
+#define mod 1000000007
+#define S second
+#define F first
+#define nl endl
+const long long A = 100000000000000LL, N = 228228;
+
+// INPUTS
+#define read1(a) cin>>a
+#define read2(a,b) cin>>a>>b
+#define read3(a,b,c) cin>>a>>b>>c
+
+// OUTPUTS
+#define print1(a) cout<<a
+#define print2(a,b) cout<<a<<b
+#define print3(a,b,c) cout<<a<<b<<c
+#define pnl cout<<endl
+
+
+// For the fast input and outputs in cpp
+void Fast_IO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// driver code for my contests
+void solution() {
+   ll n,m=0;
+   read1(n);
+   ll a[n],b[n],c[n];
+   
+   for(int i=0;i<n;i++){
+        cin>>a[i];
+        a[i] = (20)*a[i];
+    }
+        
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+        b[i] = (-10)*(b[i]);
+    }
+        
+    for(int i=0;i<n;i++){
+        c[i] = a[i]+b[i];
+        if(c[i] < 0) c[i] = 0;
+        if(c[i] > m) m = c[i];
+    }
+        
+    print1(m);
+    pnl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    Fast_IO();
+    // int t = 1; // for 1 test case
+    int tt; read1(tt);
+    while (tt--) {
+        solution();
+    }
+    return 0;
+}
